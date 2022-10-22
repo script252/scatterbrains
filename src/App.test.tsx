4c47f51cf8823a1635c0ebc8e9651b9b690bb27e
Andrew Closson <script252@gmail.com>
2022-10-22 17:01:25 -0500

    Fixed app test.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+const basename = process.env.PUBLIC_URL;
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(    
+  <BrowserRouter basename={`${basename}`}>
+  <App />
+  </BrowserRouter>);
   expect(true).toBeTruthy();//.toBeInTheDocument();
 });
