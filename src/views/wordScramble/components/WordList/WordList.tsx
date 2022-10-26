04dc333d09e1ce75f720206518d635c803130740
Andrew Closson <script252@gmail.com>
2022-10-26 17:30:03 -0500

    Success/error visual feedback. Word list auto scroll. (#13)

--- src/views/wordScramble/components/WordList/WordList.tsx
+++ src/views/wordScramble/components/WordList/WordList.tsx
@@ -1,10 +1,19 @@
 import { Box, SimpleGrid } from "@chakra-ui/layout";
-import React from "react";
+import React, {useLayoutEffect, useRef} from "react";
 
 function WordList(props: any) {
+
+    const scrollBox = useRef<HTMLDivElement>(null);
+
+    useLayoutEffect(()=> {
+        if(scrollBox?.current !== null) {
+            scrollBox.current.scrollTop = 1000;
+        }
+    }, [props.children]);
+
     return (
 
-            <Box bg='gray.800' w="100%" h="10rem" overflowY="auto">
+            <Box ref={scrollBox} bg='gray.800' w="100%" h="10rem" overflowY="auto" borderRadius="0.5rem" scrollBehavior="auto">
                 <SimpleGrid columns={[2, 2, 2]} spacing={0}>
                     {React.Children.map(props.children, (word, index) => {
                         return (
