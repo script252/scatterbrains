5ca11b8f7562789dd94e7f64732f2610e19a4e8e
Andrew Closson <script252@gmail.com>
2022-10-26 11:02:34 -0500

    Shrunk word list container to better fit mobile.

--- src/views/wordScramble/components/WordList/WordList.tsx
+++ src/views/wordScramble/components/WordList/WordList.tsx
@@ -4,8 +4,8 @@ import React from "react";
 function WordList(props: any) {
     return (
 
-            <Box bg='gray.800' w="100%" h="15rem" overflowY="auto">
-                <SimpleGrid columns={[1, 2, 2]} spacing={0}>
+            <Box bg='gray.800' w="100%" h="10rem" overflowY="auto">
+                <SimpleGrid columns={[2, 2, 2]} spacing={0}>
                     {React.Children.map(props.children, (word, index) => {
                         return (
                             <Box>{word}</Box>
