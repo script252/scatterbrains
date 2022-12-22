e488895cd5453be394d7974832b84342591f13f7
Andrew Closson <script252@gmail.com>
2022-12-22 04:15:11 -0600

    Polished support link.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import {useLocation} from 'react-router-dom';
 import './nav-bar.scss';
 
-import { Flex, Button, Menu, MenuButton, MenuList, Spacer,/*, Link*/ } from '@chakra-ui/react';
+import { Flex, Button, Menu, MenuButton, MenuList, Spacer, Box/*, Link*/ } from '@chakra-ui/react';
 import { ChevronDownIcon } from '@chakra-ui/icons';
+import { SiBuymeacoffee } from 'react-icons/si';
 
 function NavBar(props: any) {
 
@@ -12,7 +13,8 @@ function NavBar(props: any) {
     
     return (
         <Flex bg="gray.700" width="100%">
-            <a href="https://www.buymeacoffee.com/andrewclosson" className="coffee-link"><img alt="Thanks for your support!" src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=andrewclosson&button_colour=40DCA5&font_colour=ffffff&font_family=Arial&outline_colour=000000&coffee_colour=FFDD00" /></a>
+            <Box m="1"><a href="https://www.buymeacoffee.com/andrewclosson" target="_blank" rel="noreferrer"><Button leftIcon={<SiBuymeacoffee/>} h="var(--chakra-sizes-10)" colorScheme="teal">Buy me a coffee</Button></a></Box>
+            
             <Spacer />
                 <Button m="1" as={Button} colorScheme="gray" hidden={!showNewGameButton || location.pathname === '/'}
                     onClick={onNewGame}>New Game</Button>
